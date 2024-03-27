7660a88dffbf64227bcf332d021c5a878db0857d
The Enthusiast <whitedronetech@gmail.com>
2024-03-27 02:37:38 +0100

    changed system payload

--- src/types/utils/endpoint-payloads/manage-system-preference.payloads.ts
+++ src/types/utils/endpoint-payloads/manage-system-preference.payloads.ts
@@ -1,12 +1,12 @@
 import { ISystemPreference } from "../models";
 
-export interface ICreateSystemPreference<T> {
+export interface ICreateSystemPreferencePayload<T> {
     name: string;
     slug: string;
     value: T;
 }
 
-export type IEditSystemPreference<T> = Partial<ICreateSystemPreference<T>>
+export type IEditSystemPreferencePayload<T> = Partial<ICreateSystemPreferencePayload<T>>
 
 export interface ISystemPreferenceResponse<T> {
     preference: ISystemPreference<T>
