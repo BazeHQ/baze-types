a013877dfaa58d9dfa552bb2572a8540840f5df0
The Enthusiast <whitedronetech@gmail.com>
2024-04-21 11:19:01 +0100

    chore: added status to bank account

--- src/types/merchants/models/bank-accounts.model.ts
+++ src/types/merchants/models/bank-accounts.model.ts
@@ -1,5 +1,4 @@
-import { ActiveOrInactive } from "../../../../dist";
-import { IBase } from "../../generic";
+import { ActiveOrInactive, IBase } from "../../generic";
 
 export interface IBankAccount extends IBase {
     codes: {
